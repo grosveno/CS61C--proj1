5ad58d3ae209105357b5fe42dad2ac125ba4b829
yan <647979893@qq.com>
2024-10-19 16:49:56 +0800

    complete part A1

--- imageloader.c
+++ imageloader.c
@@ -25,17 +25,67 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
-	//YOUR CODE HERE
+	FILE* input = fopen(filename, "r");
+	// read mode from the first line
+	char mode[3];
+	fscanf(input, "%s", mode);
+	// read row and col from the second line
+	uint32_t row, col;
+	fscanf(input, "%u %u", &row, &col);
+	// read 255 from the third line
+	int rate;
+	fscanf(input, "%d", &rate);
+	//construct an Image object
+	Image* images = (Image*)malloc(sizeof(Image));
+	images->rows = row;
+	images->cols = col;
+	images->image = (Color**)malloc(row * sizeof(Color*));
+	for (int i = 0; i < row; i++) {
+		images->image[i] = (Color*)malloc(col * sizeof(Color));
+	}
+	// read images from next lines
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			uint8_t R, G, B;
+			fscanf(input, "%hhu %hhu %hhu", &R, &G, &B);
+			images->image[i][j].R = R;
+			images->image[i][j].G = G;
+			images->image[i][j].B = B;
+		}
+	}
+	fclose(input);
+	return images;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	printf("%s\n", "P3");
+	uint32_t row = image->rows;
+	uint32_t col = image->cols;
+	printf("%u %u\n", row, col);
+	printf("%d\n", 255);
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			int R = image->image[i][j].R;
+			int G = image->image[i][j].G;
+			int B = image->image[i][j].B;
+			printf("%3d %3d %3d", R, G, B);
+			if (j != col - 1) {
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	int row = image->rows;
+	for (int i = 0; i < row; i++) {
+		free(image->image[i]);
+	}
+	free(image->image);
+	free(image);
 }
