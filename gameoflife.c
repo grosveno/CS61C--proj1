6d4327e113142344669161db27ee3cc13b643c1e
yan <647979893@qq.com>
2024-10-21 23:04:31 +0800

    complete partB

--- gameoflife.c
+++ gameoflife.c
@@ -17,19 +17,78 @@
 #include <inttypes.h>
 #include "imageloader.h"
 
+int dis_x[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dis_y[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
 //Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
 //Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
-	//YOUR CODE HERE
+	Color* cell = (Color*)malloc(sizeof(Color));
+	int isAliveR = image->image[row][col].R == 255;
+	int isAliveG = image->image[row][col].G == 255;
+	int isAliveB = image->image[row][col].B == 255;
+	int numR = 0;
+	int numG = 0;
+	int numB = 0;
+	for (int i = 0; i < 8; i++) {
+		int newRow = (dis_y[i] + row) % row;
+		int newCol = (dis_x[i] + col) % col;
+		if (image->image[newRow][newCol].R == 255) {
+			numR++;
+		}
+		if (image->image[newRow][newCol].G == 255) {
+			numG++;
+		}
+		if (image->image[newRow][newCol].B == 255) {
+			numB++;
+		}
+	}
+	int biasR = 9 * isAliveR + numR;
+	int biasG = 9 * isAliveG + numG;
+	int biasB = 9 * isAliveB + numB;
+	if (rule & (1 << biasR)) {
+		cell->R = 255;
+	}else {
+		cell->R = 0;
+	}
+	
+	if (rule & (1 << biasG)) {
+		cell->G = 255;
+	}else {
+		cell->G = 0;
+	}
+
+	if (rule & (1 << biasB)) {
+		cell->B = 255;
+	}else {
+		cell->B = 255;
+	}
+	return cell;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
 //You should be able to copy most of this from steganography.c
 Image *life(Image *image, uint32_t rule)
 {
-	//YOUR CODE HERE
+	Image* new_image = (Image*)malloc(sizeof(Image));
+	int row = image->rows;
+	int col = image->cols;
+	new_image->rows = row;
+	new_image->cols = col;
+	new_image->image = (Color**)malloc(row * sizeof(Color*));
+	for (int i = 0; i < row; i++) {
+		new_image->image[i] = (Color*)malloc(col * sizeof(Color));
+	}
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			Color* pixel = evaluateOneCell(image, i, j, rule);
+			new_image->image[i][j] = *pixel;
+			free(pixel);
+		}
+	}
+	return new_image;
 }
 
 /*
@@ -49,5 +108,19 @@ You may find it useful to copy the code from steganography.c, to start.
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	if (argc < 3) {
+		printf("usage: ./gameOfLife filename rule\n");
+		printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+		printf("rule is a hex number beginning with 0x; Life is 0x1808.\n");
+		exit(-1);
+	}
+	char* filename = argv[1];
+	Image* image = readData(filename);
+	char* ruleOfString = argv[2];
+	int rule = strtol(ruleOfString, NULL, 10);
+	Image* newImage = life(image, rule);
+	writeData(newImage);
+	freeImage(image);
+	freeImage(newImage);
+	return 0;
 }
